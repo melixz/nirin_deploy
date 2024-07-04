e310e0484948ecef87bf86f85ed737c275e5ed85
Vlad <dr.melix@gmail.com>
2024-07-04 14:24:43 +0300

    Исправили баг с айфонами

--- meetandgreet/frontend/mag-app/src/components/MainPage/Main/MapSection/Video/Video.jsx
+++ meetandgreet/frontend/mag-app/src/components/MainPage/Main/MapSection/Video/Video.jsx
@@ -3,5 +3,5 @@ import styles from "./Video.module.css";
 import src from "../../../../../assets/1920px_bgr_map.webm";
 
 export default function Video() {
-  return <video src={src} className={styles.video} autoPlay loop muted></video>;
+  return <video src={src} className={styles.video} autoPlay loop muted playsInline></video>;
 }
